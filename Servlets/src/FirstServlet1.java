7ffb7991dc2fe8804be614422751612d7ccf1844
Ankitha <ankitha7698@gmail.com>
2018-02-28 16:09:50 +0530

    Add files via upload

---
+++ Servlets/src/FirstServlet1.java
@@ -0,0 +1,29 @@
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+  
+public class FirstServlet1 extends HttpServlet {  
+  
+  public void doGet(HttpServletRequest request, HttpServletResponse response){  
+    try{  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("userName");  
+    out.print("Welcome "+n);  
+  
+    Cookie ck=new Cookie("uname",n);//creating cookie object  
+    response.addCookie(ck);//adding cookie in the response  
+  
+    //creating submit button  
+    out.print("<form action='http://172.20.0.221:8080/122/secondS1'>");  
+    out.print("<input type='submit' value='go'>");  
+    out.print("</form>");  
+          
+    out.close();  
+  
+        }catch(Exception e){System.out.println(e);}  
+  }  
+}  
