8fcd45a4aa27e0b642f29214bb44ae2086788b92
Ankitha <ankitha7698@gmail.com>
2018-03-07 16:02:34 +0530

    added request Dispatcher

---
+++ Servlets/WEB-INF/src/WelcomeUser.java
@@ -0,0 +1,21 @@
+import java.io.*; 
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+ 
+public class WelcomeUser extends HttpServlet { 
+ 
+ public void doGet(HttpServletRequest request, 
+ HttpServletResponse response) 
+ throws ServletException, IOException 
+ { 
+ 
+    response.setContentType("text/html"); 
+    PrintWriter pwriter = response.getWriter(); 
+ 
+    String name=request.getParameter("uname"); 
+    pwriter.print("Hello "+name+"!");
+    pwriter.print(" Welcome to Beginnersbook.com"); 
+ } 
+ 
+} 
+
