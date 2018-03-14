873f22ea61918ae93833482a28c1f5375d056298
Ankitha <ankitha7698@gmail.com>
2018-03-14 15:59:52 +0530

    Added Registration and Validation using JSP

---
+++ JSP Programs/src/RegistrationController.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**Servlet implementation class UserDataServlet*/
+public class RegistrationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+          
+	public RegistrationController() {
+	super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("fullname");
+		String Addr = request.getParameter("address");
+		String age = request.getParameter("age");
+		String Qual = request.getParameter("qual");
+		String Persent = request.getParameter("percent");
+		String Year = request.getParameter("yop");
+		if(name.isEmpty()||Addr.isEmpty()||age.isEmpty()||Qual.isEmpty()||Persent.isEmpty()||Year.isEmpty())
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("registration.jsp");
+			out.println("<font color=red>Please fill all the fields</font>");
+			rd.include(request, response);
+		}
+		else
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+}
